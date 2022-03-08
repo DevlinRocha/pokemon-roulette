375b1389fe7940cd489b9132c26458484809d010
Devlin <devlincodes@gmail.com>
2022-03-07 21:49:11 -0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      title: "Who's that Pokémon?",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
