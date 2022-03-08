02a44d8db483e1c3664b331b1a1b21bdfdf74f92
Devlin <devlincodes@gmail.com>
2022-03-08 02:38:06 -0800

    Display silhouette of a random Pokémon

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,56 @@
 <script lang="ts">
-export default {
+import { defineComponent } from "vue";
+import Pokemon from "./components/Pokemon.vue";
+
+export interface PokemonData {
+  name: string;
+  img: string;
+}
+
+export default defineComponent({
+  async mounted() {
+    const pokemonData = await this.getPokemon(this.getRandomId(this.pokedex));
+
+    this.pokemon = {
+      name: pokemonData.name,
+      img: pokemonData.sprites.front_default,
+    };
+  },
+
   data() {
     return {
       title: "Who's that Pokémon?",
+
+      pokedex: 898, // Maximum Pokémon ID,
+
+      pokemon: { name: "", img: "" }, // Current Pokémon
     };
   },
-};
+
+  methods: {
+    async getPokemon(id: number) {
+      if (id > this.pokedex)
+        throw new Error("Pokémon not found. There are 898 known Pokémon.");
+
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+      return await response.json();
+    },
+
+    getRandomId(max: number) {
+      return Math.floor(Math.random() * max) + 1;
+    },
+  },
+  components: { Pokemon },
+});
 </script>
 
 <template>
   <h1>{{ title }}</h1>
+
+  <Pokemon :pokemon="pokemon" />
 </template>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import { PokemonData } from "../App.vue";
+
+export default defineComponent({
+  props: {
+    pokemon: {
+      type: Object as PropType<PokemonData>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<template>
+  <img class="unknown" :src="$props.pokemon.img" />
+</template>
+
+<style scoped>
+.unknown {
+  filter: brightness(0);
+}
+</style>
