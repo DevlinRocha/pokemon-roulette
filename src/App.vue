7fcbb25c790b27dbc178b143bc267e0d6c1e6071
Devlin <devlincodes@gmail.com>
2022-03-16 21:19:09 -0700

    Capitalize first letter of each word of user input

--- src/App.vue
+++ src/App.vue
@@ -147,7 +147,12 @@ export default defineComponent({
     },
 
     capitalize(str: string) {
-      return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+      return str
+        .split(" ")
+        .map(
+          (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        )
+        .join(" ");
     },
 
     focusInput() {
